6ff10f1bfcfc2f28a0cb561f5edd843bf717d9e1
thiagopicorelli <thiagopicorellichalet@gmail.com>
2023-08-22 18:04:17 -0300

    Completa requisito 4

--- spotnews/urls.py
+++ spotnews/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from news_rest.views import index
+from news.views import index
 
 urlpatterns = [
     path("admin/", admin.site.urls),
