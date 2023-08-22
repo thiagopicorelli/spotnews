259365adc4eb1c789737ec278766c2157f5b61fd
thiagopicorelli <thiagopicorellichalet@gmail.com>
2023-08-22 17:49:32 -0300

    Monta lista de noticias inicial na pagina

--- news_rest/views/__init__.py
+++ news_rest/views/__init__.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from news.models import News
 
 
 def index(request):
-    return render(request, 'home.html')
+    context = {"news": News.objects.all()}
+    return render(request, 'home.html', context)
