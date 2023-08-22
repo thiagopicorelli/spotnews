c52a7a7d30d1ac224d9a08ef88201bd4620f3426
thiagopicorelli <thiagopicorellichalet@gmail.com>
2023-08-22 17:25:04 -0300

    Inicia template e conecta a rota

--- spotnews/urls.py
+++ spotnews/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from news_rest.views import index
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", index, name="home-page")
 ]
 
 if settings.DEBUG:
