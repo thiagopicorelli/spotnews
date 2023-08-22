7bed14fbbb249cf885499a31a64117a04871e139
thiagopicorelli <thiagopicorellichalet@gmail.com>
2023-08-22 18:27:38 -0300

    Copia as imagens static para o root da pasta para aparecer na homepage com o link correto

--- news_rest/views/__init__.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from news.models import News
-
-
-def index(request):
-    context = {"news": News.objects.all()}
-    return render(request, 'home.html', context)
