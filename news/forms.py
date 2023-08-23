71d6361fe29a7757734057883dd367752815966f
thiagopicorelli <thiagopicorellichalet@gmail.com>
2023-08-23 19:56:29 -0300

    Completa variáveis do forms, só faltando corrigir as imagens

--- news/forms.py
+++ news/forms.py
@@ -1,13 +1,23 @@
+import datetime
 from django import forms
 from news.models.user_model import Users
+from news.models.category_model import Categories
 
 class CreateCategoryForm(forms.Form):
     name = forms.CharField(label="Nome", max_length=200, required=True)
 
 class CreateNewsForm(forms.Form):
-  authors = Users.objects.all()
-  authorChoices = map(lambda e: (e.id, e.name), authors)
+
+  def __init__(self, *args, **kwargs):
+    super(CreateNewsForm, self).__init__(*args, **kwargs)
+    authorChoices = map(lambda e: (e.id, e.name), Users.objects.all())
+    categoryChoices = map(lambda e: (e.id, e.name), Categories.objects.all())
+    self.fields["author"] = forms.ChoiceField(label="Autoria", choices=authorChoices, required=True)
+    self.fields["categories"] = forms.MultipleChoiceField(label="Categorias", choices=categoryChoices, widget=forms.CheckboxSelectMultiple, required=True)
 
   title = forms.CharField(label="Título", max_length=200, required=True)
   content = forms.CharField(label="Conteúdo", widget=forms.Textarea, required=True)
-  author = forms.ChoiceField(label="Autoria", choices=authorChoices)
+  author = forms.ChoiceField(label="Autoria", required=True)
+  created_at = forms.DateField(label="Criado em", widget=forms.DateInput(attrs={"type": "date"}), initial=datetime.date.today().strftime('%Y-%m-%d'), required=True)
+  image = forms.ImageField(label="URL da Imagem", required=False)
+  categories = forms.MultipleChoiceField(label="Categorias", widget=forms.CheckboxSelectMultiple, required=True)
--- news/views.py
+++ news/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from news.models import News, Categories
+from news.models import News, Categories, Users
 from news.forms import CreateCategoryForm, CreateNewsForm
 
 
@@ -24,6 +24,20 @@ def categories_form(request):
 
 def news_form(request):
     form = CreateNewsForm()
+    if request.method == "POST":
+        form = CreateNewsForm(request.POST)
+        if form.is_valid():
+            cleanForm = form.cleaned_data
+            news = News(
+                title = cleanForm["title"],
+                content = cleanForm["content"],
+                author = Users.objects.get(id=cleanForm["author"]),
+                created_at = cleanForm["created_at"],
+                image = cleanForm["image"]
+            )
+            news.save()
+            news.categories.set(Categories.objects.filter(id__in=cleanForm["categories"]))
+            return redirect("home-page")
     context = {"form": form}
     return render(request, "news_form.html", context)
     
